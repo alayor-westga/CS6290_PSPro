cb131556281bbb89cbfb9edff8fc4b45aa32800f
Mike Hamel <mhamel1@my.westga.edu>
2021-06-06 16:38:26 -0700

    added comments to Model

--- PSPro/Model/OfficerComboBox.cs
+++ PSPro/Model/OfficerComboBox.cs
@@ -8,12 +8,24 @@ namespace PSPro.Model
 {
     public class OfficerComboBox
     {
+        /// <summary>
+        /// Personnel ID associated with Officer Combo Box
+        /// </summary>
         public int PersonnelID { get; set; }
 
+        /// <summary>
+        /// First Name associated with Officer Combo Box
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last Name associated with Officer Combo Box
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Display Name for Officer Combo Box
+        /// </summary>
         public string DisplayName
         {
             get
