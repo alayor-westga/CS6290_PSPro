673a6ec0bdf1fde8868c2580a8cec3b352878080
Mike Hamel <mhamel1@my.westga.edu>
2021-06-06 12:03:47 -0700

    added DAL and Controller classes for Supervisor and created shell NewComplaintForm

---
+++ PSPro/Controller/SupervisorController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSPro.Controller
+{
+    
+    public class SupervisorController    
+    {
+        private readonly SupervisorDAL supervisorSource;
+    }
+}
---
+++ PSPro/DAL/SupervisorDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSPro.DAL
+{
+    class SupervisorDAL
+    {
+
+    }
+}
