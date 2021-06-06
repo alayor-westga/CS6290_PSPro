bf947d1e3f6b201dcdedfaad4f2100d44197bec0
Mike Hamel <mhamel1@my.westga.edu>
2021-06-06 16:36:39 -0700

    implemented Model for OfficerComboBox

---
+++ PSPro/Model/OfficerComboBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSPro.Model
+{
+    public class OfficerComboBox
+    {
+        public int PersonnelID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DisplayName
+        {
+            get
+            {
+                return LastName + ", " + FirstName + " (" + PersonnelID + ")";
+            }
+        }
+
+    }
+}
