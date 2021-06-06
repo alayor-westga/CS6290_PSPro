b8c8ec4eeb7c7713cde797768e06bfdaac9f7e79
Alonso Ayala Ortega <aayalao1@my.westga.edu>
2021-06-06 17:22:49 -0600

    Rename PSPro2 to PSPro

--- PSPro/Properties/AssemblyInfo.cs
+++ PSPro/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PSPro2")]
+[assembly: AssemblyTitle("PSPro")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PSPro2")]
+[assembly: AssemblyProduct("PSPro")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- PSPro/Properties/Resources.Designer.cs
+++ PSPro/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace PSPro2.Properties
+namespace PSPro.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace PSPro2.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PSPro2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PSPro.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
