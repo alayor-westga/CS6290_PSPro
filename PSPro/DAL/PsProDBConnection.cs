2e35f67020d1a3e914c66aa5b3c7d7651055b602
Alonso Ayala Ortega <aayalao1@my.westga.edu>
2021-06-06 12:01:18 -0600

    Ammend UI Mockups. Add DB connection string.

---
+++ PSPro/DAL/PsProDBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSPro.DAL
+{
+    /// <summary>
+    /// Get a connection object. 
+    /// </summary>
+    public class PsProDBConnection
+    {
+        /// <summary>
+        /// It returns a SQL connection to the pspro DB.
+        /// </summary>
+        /// <returns>A SQL connection</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Server=tcp:pspro.database.windows.net,1433;Initial Catalog=pspro;Persist Security Info=False;User ID=winforms;Password=Winf0rms;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
