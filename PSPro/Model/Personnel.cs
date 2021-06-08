1ce1cb71fca3ca25b3f641cf21d806bf7252811e
Alonso Ayala Ortega <aayalao1@my.westga.edu>
2021-06-08 08:45:17 -0600

    Add stored procedures to login investigators and administrators

--- PSPro/Model/Personnel.cs
+++ PSPro/Model/Personnel.cs
@@ -4,12 +4,51 @@ using System.Text;
 
 namespace PSPro.Model
 {
-    public class Supervisor : Personnel
+    public class Personnel
     {
+        /// <summary>
+        /// ID and PK for Personnel object
+        /// </summary>
+        public int PersonelID { get; set; }
 
         /// <summary>
-        /// Username for Citizen object
+        /// First Name for Citizen object
         /// </summary>
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last Name for Citizen object
+        /// </summary>
+        public string LastName { get; set;  }
+
+        /// <summary>
+        /// Gender for Citizen object
+        /// </summary>
+        public string Gender { get; set;  }
+
+        /// <summary>
+        /// Hire date for Citizen object
+        /// </summary>
+        public string HireDate { get; set;  }
+
+        /// <summary>
+        /// Birth date for Citizen object
+        /// </summary>
+        public string BirthDate { get; set;  }
+
+        /// <summary>
+        /// Assignment for Citizen object
+        /// </summary>
+        public string Assignment { get; set;  }
+
+        // <summary>
+        /// The full name of the clinic person.
+        /// </summary>
+        public string FullName { 
+            get 
+            {
+                return FirstName + " " + LastName;
+            }
+        }
     }
 }
--- PSPro/Model/Supervisor.cs
+++ PSPro/Model/Supervisor.cs
@@ -4,51 +4,12 @@ using System.Text;
 
 namespace PSPro.Model
 {
-    public class Personnel
+    public class Supervisor : Personnel
     {
-        /// <summary>
-        /// ID and PK for Personnel object
-        /// </summary>
-        public int PersonelID { get; set; }
 
         /// <summary>
-        /// First Name for Citizen object
+        /// Username for Citizen object
         /// </summary>
-        public string FirstName { get; set; }
-
-        /// <summary>
-        /// Last Name for Citizen object
-        /// </summary>
-        public string LastName { get; set;  }
-
-        /// <summary>
-        /// Gender for Citizen object
-        /// </summary>
-        public string Gender { get; set;  }
-
-        /// <summary>
-        /// Hire date for Citizen object
-        /// </summary>
-        public string HireDate { get; set;  }
-
-        /// <summary>
-        /// Birth date for Citizen object
-        /// </summary>
-        public string BirthDate { get; set;  }
-
-        /// <summary>
-        /// Assignment for Citizen object
-        /// </summary>
-        public string Assignment { get; set;  }
-
-        // <summary>
-        /// The full name of the clinic person.
-        /// </summary>
-        public string FullName { 
-            get 
-            {
-                return FirstName + " " + LastName;
-            }
-        }
+        public string UserName { get; set; }
     }
 }
