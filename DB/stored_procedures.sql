47039d75dce07f9b483a8c30efbfc8dabaed78ae
mhamel1 <68163579+mhamel1@users.noreply.github.com>
2021-06-07 22:08:17 -0700

    Update stored_procedures.sql
    Added AddIncident Stored Procedure

--- DB/stored_procedures.sql
+++ DB/stored_procedures.sql
@@ -29,3 +29,28 @@ GRANT EXECUTE ON
 GetAllOfficersForComboBox
 TO winforms;
 GO
+
+
+
+DROP PROCEDURE IF EXISTS AddIncident;
+GO
+CREATE PROCEDURE AddIncident 
+	@citizen_id int, 
+	@first_name varchar, 
+	@last_name varchar, 
+	@address1 varchar, 
+	@address2 varchar, 
+	@city varchar, 
+	@state char(2), 
+	@zipcode varchar, 
+	@phone varchar, 
+	@email varchar
+AS
+SET NOCOUNT ON;
+
+INSERT Citizens (citizen_id, first_name, last_name, address1, address2, city, state, zipcode, phone, email)
+VALUES (@citizen_id, @first_name, @last_name, @address1, @address2, @city, @state, @zipcode, @phone, @email)
+GO
+GRANT EXECUTE ON AddIncident 
+    TO winforms;  
+GO 
