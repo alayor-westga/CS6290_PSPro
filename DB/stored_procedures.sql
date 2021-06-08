ef4d8d132694ba18e99a8579f9abc50f5aaf2013
mhamel1 <68163579+mhamel1@users.noreply.github.com>
2021-06-07 21:20:07 -0700

    Update stored_procedures.sql
    Added GetAllOfficersForComboBox

--- DB/stored_procedures.sql
+++ DB/stored_procedures.sql
@@ -10,4 +10,22 @@ WHERE username = @UserName AND password = @Password;
 GO
 GRANT EXECUTE ON GetSupervisorByUserNameAndPassword 
     TO winforms;  
-GO  
+GO 
+
+
+
+DROP PROCEDURE IF EXISTS GetAllOfficersForComboBox;
+GO
+CREATE PROCEDURE GetAllOfficersForComboBox
+AS
+SET NOCOUNT ON;
+
+    SELECT p.last_name, p.first_name, p.personnel_id
+	FROM Officers o, Personnel p
+	WHERE o.personnel_id = p.personnel_id
+	ORDER BY p.last_name;
+GO
+GRANT EXECUTE ON
+GetAllOfficersForComboBox
+TO winforms;
+GO
