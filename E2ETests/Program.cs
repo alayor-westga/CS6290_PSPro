878a38957ea6141f60664ea146955aabf4fbea7a
Alonso Ayala Ortega <aayalao1@my.westga.edu>
2021-06-09 08:15:31 -0600

    Add initial E2E Test Project with Fla UI.

---
+++ E2ETests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using FlaUI.UIA2;
+
+namespace E2ETests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "PSPro.exe";
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\..\PSPro\bin\Debug\", fileName);
+            var app = FlaUI.Core.Application.Launch(path);
+            using (var automation = new UIA2Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                var loginButton = window.FindFirstDescendant(cf => cf.ByText("Login"));
+                loginButton?.Click();
+            }
+            app.Close();
+        }
+    }
+}
