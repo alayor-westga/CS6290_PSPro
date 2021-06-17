2e1775f59d28b60ca25be693edafae9706237a4e
Mike Hamel <mhamel1@my.westga.edu>
2021-06-16 23:01:38 -0700

    Added comments to class and public methods

--- PSPro/Controller/CitizenController.cs
+++ PSPro/Controller/CitizenController.cs
@@ -1,27 +1,42 @@
 ﻿using PSPro.DAL;
 using PSPro.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PSPro.Controller
 {
+    /// <summary>
+    /// Model for Citizen
+    /// </summary>
     class CitizenController      
     {
         CitizenDAL citizenSource = new CitizenDAL();
 
+        /// <summary>
+        /// Search for Citizen by name
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns>a List of Citizen objects</returns>
         public List<Citizen> SearchByName(string firstName, string lastName)
         {
             return this.citizenSource.SearchByName(firstName, lastName);
         }
 
+        /// <summary>
+        /// Search for Citizen by phone
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns>a List of Citizen objects</returns>
         public List<Citizen> SearchByPhone(string phone)
         {
             return this.citizenSource.SearchByPhone(phone);
         }
 
+        /// <summary>
+        /// Search for Citizen by email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>a list of Citizen objects</returns>
         public List<Citizen> SearchByEmail(string email)
         {
             return this.citizenSource.SearchByEmail(email);
