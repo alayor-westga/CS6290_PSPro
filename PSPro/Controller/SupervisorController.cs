f206de32a5a0e8cc95a7dc1b3a6944caae78d9c9
Mike Hamel <mhamel1@my.westga.edu>
2021-06-16 23:12:46 -0700

    Added comments to class and public methods

--- PSPro/Controller/SupervisorController.cs
+++ PSPro/Controller/SupervisorController.cs
@@ -6,6 +6,10 @@ using PSPro.Model;
 
 namespace PSPro.Controller
 {
+
+    /// <summary>
+    /// Controller class for Supervisor
+    /// </summary>
     public class SupervisorController
     {
         private readonly SupervisorDAL supervisorSource;
@@ -20,16 +24,30 @@ namespace PSPro.Controller
             this.supervisorSource = supervisorDAL;
         }
 
+        /// <summary>
+        /// Adds Citizen object to Citizen table and adds a new Complaint object to Complaint table
+        /// </summary>
+        /// <param name="citizen"></param>
+        /// <param name="complaint"></param>
         public void AddCitizenAndComplaint(Citizen citizen, Complaint complaint)
         {
             this.supervisorSource.AddCitizenAndComplaint(citizen, complaint);
         }
 
+        /// <summary>
+        /// Adds new Complaint object to Complaint table
+        /// </summary>
+        /// <param name="complaint"></param>
         public void AddComplaint(Complaint complaint)
         {
             this.supervisorSource.AddComplaint(complaint);
         }
 
+        /// <summary>
+        /// Gets Citizen object from the db by citizenID
+        /// </summary>
+        /// <param name="citizenID"></param>
+        /// <returns>Citizen object</returns>
         public Citizen GetCitizen(int citizenID)
         {
             if (citizenID < 0)
@@ -39,6 +57,12 @@ namespace PSPro.Controller
             return supervisorSource.GetCitizen(citizenID);
         }
 
+        /// <summary>
+        /// Attempts to update Citizen to db and returns true if successful, false otherwise
+        /// </summary>
+        /// <param name="citizen"></param>
+        /// <param name="updatedCitizen"></param>
+        /// <returns>True if Citizen is updated, false otherwise</returns>
         public bool UpdateCitizen(Citizen citizen, Citizen updatedCitizen)
         {
             if (citizen == null)
