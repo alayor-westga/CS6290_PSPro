e174db216d0b1570cd31fe19446e60fbaf0c776a
Mike Hamel <mhamel1@my.westga.edu>
2021-07-07 11:20:40 -0700

    implemented statistical report model

---
+++ PSPro/Model/StatReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSPro.Model
+{
+    class StatReport
+    {
+        public int TotalComplaints { get; set; }
+        public int MostCommonComplaint { get; set; }
+        public int PercentOfSustainedComplaints { get; set; }
+        public int MostFrequentlyNamedOfc { get; set; }
+        public int LeastFrequentlyNamedOfc { get; set; }
+    }
+}
